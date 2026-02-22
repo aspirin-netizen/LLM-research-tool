aa686e115158468f93bd688858f13925c419bc40
aspirin <aspirinbin@gmail.com>
2026-02-22 20:23:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ except Exception:
 if "GEMINI_API_KEY" in st.secrets:
     genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
     model = genai.GenerativeModel(
-        model_name='gemini-1.5-flash',
+        model_name='models/gemini-1.5-flash',
         system_instruction=SYSTEM_PROMPT
     )
 else:
